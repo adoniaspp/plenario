6660eb017e2e53adc466323476cdfa08ca124725
adoniaspires <latpar69>
2020-02-27 17:37:58 -0300

    inicial

---
+++ lib/Style/Colors.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+const principal1  = const Color.fromRGBO(174, 51, 63, 1.0); // Vermelho grená
+const principal2  = const Color.fromRGBO(255, 255, 255, 1.0); // Branco
+const principal3  = const Color.fromRGBO(0, 0, 0, 1.0); // Preto
+
+const auxAzul  = const Color.fromRGBO(31, 78, 121, 1.0);
+const auxAzul80  = const Color.fromRGBO(75, 113, 147, 1.0);
+const auxAzul60  = const Color.fromRGBO(120, 148, 174, 1.0);
+const auxAzul40  = const Color.fromRGBO(165, 184, 201, 1.0);
+const auxAzul20  = const Color.fromRGBO(210, 219, 228, 1.0);
+
+const auxCinza  = const Color.fromRGBO(127, 127, 127, 1.0);
+const auxCinza80  = const Color.fromRGBO(152, 152, 152, 1.0);
+const auxCinza60  = const Color.fromRGBO(178, 178, 178, 1.0);
+const auxCinza40  = const Color.fromRGBO(204, 204, 204, 1.0);
+const auxCinza20  = const Color.fromRGBO(229, 229, 229, 1.0);
+
+const auxAmarelo  = const Color.fromRGBO(223, 218, 0, 1.0);
+const auxAmarelo80  = const Color.fromRGBO(229, 225, 51, 1.0);
+const auxAmarelo60  = const Color.fromRGBO(236, 233, 102, 1.0);
+const auxAmarelo40  = const Color.fromRGBO(242, 240, 153, 1.0);
+const auxAmarelo20  = const Color.fromRGBO(249, 248, 204, 1.0);
+
+const auxVerde  = const Color.fromRGBO(84, 130, 53, 1.0);
+const auxVerde80  = const Color.fromRGBO(84, 130, 53, 1.0);
+const auxVerde60  = const Color.fromRGBO(152, 180, 133, 1.0);
+const auxVerde40  = const Color.fromRGBO(186, 205, 174, 1.0);
+const auxVerde20  = const Color.fromRGBO(221, 230, 214, 1.0);
+
+const auxLaranja  = const Color.fromRGBO(255, 192, 0, 1.0);
+const auxLaranja80  = const Color.fromRGBO(255, 204, 51, 1.0);
+const auxLaranja60  = const Color.fromRGBO(255, 217, 102, 1.0);
+const auxLaranja40  = const Color.fromRGBO(255, 230, 153, 1.0);
+const auxLaranja20  = const Color.fromRGBO(255, 243, 204, 1.0);
+
